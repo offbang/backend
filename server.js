eab784464779dd2f4d188112cbe9a8e69f581376
offbang <bajgaming66@gmail.com>
2024-11-30 14:45:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-git clone https://github.com/offbang/backend.git
+const express = require('express');
+const { Telegraf } = require('telegraf');  // Example for Telegram bot
+const app = express();
+const port = 3000;
+
+// Setup your Telegram bot here
+const bot = new Telegraf('7405438395:AAE_lMyHZjxA3NeKWyEhXHKBuRzBG1tTWbs');
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello from server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
